2ae96d6efde8207e38330cea275b8a68c2417b3c
Pankaj Paliwal <158048038+pankaj-0301@users.noreply.github.com>
2025-07-10 15:03:00 +0530

    Add changes

--- backend-folder/main.py
+++ backend-folder/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
